3649ceefdd5db2475aab711439f6859d6a60e240
rishikesh-babu <rishikeshbabu2004@gmail.com>
2024-08-25 01:57:24 +0530

    Updated Portfolio

---
+++ script.js
@@ -0,0 +1,10 @@
+function handlervalidation() {
+    // window.location.href = "https://form-validation-demo-intraniz.netlify.app/"
+    window.open("https://form-validation-demo-intraniz.netlify.app/", '_blank')
+}
+function handlerCalculator() {
+    window.open("https://online-calculator-demo-intrainz.netlify.app/", '_blank')
+}
+function handlerShop() {
+    window.open("https://rishikesh-babu.github.io/Bootstrap-Module-Assignment/", '_blank')
+}

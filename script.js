9c2baa4985682a377ca720ebe13918816d887d1e
rishikesh-babu <rishikeshbabu2004@gmail.com>
2024-08-25 14:34:57 +0530

    Contact added

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
+document.querySelector('.cvdownload').addEventListener('click', cvhandler)
+
+function cvhandler() {
+    alert('Cv is not updated, will add later')
+}
 function handlervalidation() {
     // window.location.href = "https://form-validation-demo-intraniz.netlify.app/"
     window.open("https://form-validation-demo-intraniz.netlify.app/", '_blank')
 }
+
 function handlerCalculator() {
     window.open("https://online-calculator-demo-intrainz.netlify.app/", '_blank')
 }
+
 function handlerShop() {
     window.open("https://rishikesh-babu.github.io/Bootstrap-Module-Assignment/", '_blank')
 }
+
+function validation() {
+    event.preventDefault();
+    setTimeout(() => {
+        alert("Thanks, Will contact you")
+    },500);
+    document.querySelector('.resultvalidation').innerHTML = 'Your message has been send successfully'
+}
+
